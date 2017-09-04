74c2cad46a56d5349c91a66b8dbd111cbf4f31a0
Santiago Mendoza <smendoza@isf.com.co>
2017-09-04 14:48:27 -0500

    Add courses tags and filtering by tags

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import About from './components/About';
 import Experience from './components/Experience';
 import Education from './components/Education';
-import Certificate from './components/Certificate';
+import Course from './components/Course';
 // import Skills from './components/Skills';
 
 class App extends Component {
   render() {
-
     const personEnglish = {
-      avatar: 'https://es.gravatar.com/userimage/19167007/631baacc80a5219a3ad17eee332cbe4e.jpg?size=700',
+      avatar:
+        'https://es.gravatar.com/userimage/19167007/631baacc80a5219a3ad17eee332cbe4e.jpg?size=700',
       name: 'Santiago Mendoza',
       profession: 'Software Engineer',
-      bio: 'Software Engineer at ISF de Colombia. Christian. Passionate about God, Digital Marketing and personal finances.',
+      bio:
+        'Software Engineer at ISF de Colombia. Christian. Passionate about God, Digital Marketing and personal finances.',
       address: 'Cartagena, Colombia.',
       email: 'contacto@santiagomendoza.org',
       social: [
@@ -23,54 +24,168 @@ class App extends Component {
       ],
       experience: [
         {
-          jobTitle: 'Software Engineer', company: 'ISF', startDate: 'Jan 2017', endDate: 'Present',
+          jobTitle: 'Software Engineer',
+          company: 'ISF',
+          startDate: 'Jan 2017',
+          endDate: 'Present',
           jobDescription: `Desarrollo de Software utilizando el siguiente Stack:
           Node.js, React.js, Oracle, MongoDB, ElasticSearch, Git, Github.
           Siguiendo la metodología Scrum.`
         },
         {
-          jobTitle: 'Tech Leader', company: 'Universidad Tecnológica de Bolívar', startDate: 'Jul 2015', endDate: 'Dec 2016',
+          jobTitle: 'Tech Leader',
+          company: 'Universidad Tecnológica de Bolívar',
+          startDate: 'Jul 2015',
+          endDate: 'Dec 2016',
           jobDescription: `Administración de Servidores Linux.
           Desarrollo e implementación de aplicaciones utilizando JavaScript, HTML5, CSS3, JavaScript, PHP, y Frameworks como Laravel, AngularJS.
           Administración y soporte de LMS Moodle.`
         },
         {
-          jobTitle: 'Systems Engineering Practitioner', company: 'Intertug', startDate: 'Dec 2014', endDate: 'Jun 2015',
+          jobTitle: 'Systems Engineering Practitioner',
+          company: 'Intertug',
+          startDate: 'Dec 2014',
+          endDate: 'Jun 2015',
           jobDescription: `Diseño y desarrollo de aplicación web para Tracking.
           Desarrollo de scripts en Python para administración y mantenimiento de Raspberrys remotos.`
         },
         {
-          jobTitle: 'Teacher', company: 'Interlat', startDate: 'Apr 2016', endDate: 'Nov 2016',
+          jobTitle: 'Teacher',
+          company: 'Interlat',
+          startDate: 'Apr 2016',
+          endDate: 'Nov 2016',
           jobDescription: `Docente del módulo y curso especializado
           "SEO: Posicionamiento en buscadores" del Diplomado en Marketing Digital de Interlat en convenio con la Universidad Pontificia Bolivariana.`
-        },
+        }
       ],
       education: [
         {
-          degree: 'Minor, Cloud Computing Infrastructure', institution: 'Universidad Tecnológica de Bolívar', startDate: '2014', endDate: '2014',
+          degree: 'Minor, Cloud Computing Infrastructure',
+          institution: 'Universidad Tecnológica de Bolívar',
+          startDate: '2014',
+          endDate: '2014',
           description: `Server Administration. Virtualization. Networking.`
         },
         {
-          degree: 'Systems Engineer', institution: 'Universidad Tecnológica de Bolívar', startDate: 'Feb 2011', endDate: 'Oct 2015',
+          degree: 'Systems Engineer',
+          institution: 'Universidad Tecnológica de Bolívar',
+          startDate: 'Feb 2011',
+          endDate: 'Oct 2015',
           description: `Monitor de Algoritmos 2P-2014. Representante estudiantil ante el Comité Curricular de Ingeniería de Sistemas 2014 - 2015`
-        },
+        }
       ],
       courses: [
-        { name: 'Learn and Understand NodeJS', institution: 'Udemy', date: 'Jan 2017', description: 'Dive deep under the hood of NodeJS. Learn V8, Express, the MEAN stack, core Javascript concepts, and more.' },
-        { name: 'Modern React with Redux', institution: 'Udemy', date: 'Jan 2017', description: 'Master the fundamentals of React and Redux with this tutorial as you develop apps supported by NPM, Webpack, and ES6' },
-        { name: 'M101JS: MongoDB for Node.js Developers', institution: 'MongoDB University', date: 'May 2017', description: 'Learn everything you need to know to get started building a MongoDB-based app.' },
-        { name: 'Liderazgo Empresarial', institution: 'TAB University', date: 'Aug 2017', description: 'Know the personality type and bring the whole team through the change curve.' },
-        { name: 'LFS101x.2 Introduction to Linux', institution: 'EdX', date: 'Feb 2015', description: '' },
-        { name: 'Inbound Certified', institution: 'HubSpot', date: 'Mar 2017 - May 2019', description: 'Inbound Marketing Certification' },
-        { name: 'Carrera de Administración de Servidores y DevOps', institution: 'Platzi', date: 'Mar 2016 - Mar 2017', description: '' },
-        { name: 'Curso básico de Marketing Digital', institution: 'IAB Spain', date: 'Jul 2014', description: '' },
-        { name: 'Academia de Obreros', institution: 'Iglesia Cristiana Rios De Vida', date: 'Apr 2014', description: '' },
-        { name: 'Academia Avanzada', institution: 'Iglesia Cristiana Rios De Vida', date: 'Nov 2015', description: '' },
-        { name: 'FrontEnd Developer', institution: 'Platzi', date: 'Jan 2015', description: '' },
-        { name: 'Backend Developer', institution: 'Platzi', date: 'Jan 2016', description: '' },
-        { name: 'Diplomado en Gerencia en Tecnologías en la Nube para Empresas', institution: 'Unipymes', date: 'Apr 2015', description: 'Intensidad de 30 horas' },
-
-      ],
+        {
+          name: 'Learn and Understand NodeJS',
+          institution: 'Udemy',
+          date: 'Jan 2017',
+          tags: 'javascript, nodejs',
+          description:
+            'Dive deep under the hood of NodeJS. Learn V8, Express, the MEAN stack, core Javascript concepts, and more.'
+        },
+        {
+          name: 'ES6 for Everyone',
+          institution: 'WesBos',
+          date: 'Aug 2017',
+          tags: 'javascript',
+          description: 'Modern JavaScript'
+        },
+        {
+          name: 'Writing for the web',
+          institution: 'Open2Study',
+          date: 'Sep 2017',
+          tags: 'blogging, marketing digital, seo',
+          description:
+            'Explore how writing style, web design and structure can grab the attention of and engage online readers.'
+        },
+        {
+          name: 'Modern React with Redux',
+          institution: 'Udemy',
+          date: 'Jan 2017',
+          tags: 'javascript, react, redux',
+          description:
+            'Master the fundamentals of React and Redux with this tutorial as you develop apps supported by NPM, Webpack, and ES6'
+        },
+        {
+          name: 'M101JS: MongoDB for Node.js Developers',
+          institution: 'MongoDB University',
+          date: 'May 2017',
+          tags: 'mongodb, javascript, nodejs',
+          description:
+            'Learn everything you need to know to get started building a MongoDB-based app.'
+        },
+        {
+          name: 'Liderazgo Empresarial',
+          institution: 'TAB University',
+          date: 'Aug 2017',
+          tags: 'leadership',
+          description:
+            'Know the personality type and bring the whole team through the change curve.'
+        },
+        {
+          name: 'LFS101x.2 Introduction to Linux',
+          institution: 'EdX',
+          date: 'Feb 2015',
+          tags: 'linux, sysadm',
+          description: ''
+        },
+        {
+          name: 'Inbound Certified',
+          institution: 'HubSpot',
+          date: 'Mar 2017 - May 2019',
+          tags: 'marketing digital',
+          description: 'Inbound Marketing Certification'
+        },
+        {
+          name: 'Carrera de Administración de Servidores y DevOps',
+          institution: 'Platzi',
+          date: 'Mar 2016 - Mar 2017',
+          tags: 'sysadm, devops',
+          description: ''
+        },
+        {
+          name: 'Curso básico de Marketing Digital',
+          institution: 'IAB Spain',
+          date: 'Jul 2014',
+          tags: 'marketing digital',
+          description: ''
+        },
+        {
+          name: 'Academia de Obreros',
+          institution: 'Iglesia Cristiana Rios De Vida',
+          date: 'Apr 2014',
+          tags: 'cristiano',
+          description: ''
+        },
+        {
+          name: 'Academia Avanzada',
+          institution: 'Iglesia Cristiana Rios De Vida',
+          date: 'Nov 2015',
+          tags: 'cristiano',
+          description: ''
+        },
+        {
+          name: 'FrontEnd Developer',
+          institution: 'Platzi',
+          date: 'Jan 2015',
+          tags: 'javascript',
+          description: ''
+        },
+        {
+          name: 'Backend Developer',
+          institution: 'Platzi',
+          date: 'Jan 2016',
+          tags: 'backend',
+          description: ''
+        },
+        {
+          name: 'Diplomado en Gerencia en Tecnologías en la Nube para Empresas',
+          institution: 'Unipymes',
+          date: 'Apr 2015',
+          tags: 'sysadm, devops',
+          description: 'Intensidad de 30 horas'
+        }
+      ]
       // skills: [
       //   { name: 'HTML5', percentage: '95%' },
       //   { name: 'CSS', percentage: '90%' },
@@ -79,11 +194,10 @@ class App extends Component {
       // ]
     };
 
-
     return (
       <header>
-        <div className='wrapper'>
-          <div className='sidebar'>
+        <div className="wrapper">
+          <div className="sidebar">
             <About
               avatar={personEnglish.avatar}
               name={personEnglish.name}
@@ -95,15 +209,14 @@ class App extends Component {
             />
           </div>
 
-          <div className='content-wrapper'>
-            <div className='content'>
+          <div className="content-wrapper">
+            <div className="content">
               <Experience experience={personEnglish.experience} />
               <Education education={personEnglish.education} />
-              <Certificate courses={personEnglish.courses} />
+              <Course courses={personEnglish.courses} />
               {/*<Skills skills={personEnglish.skills} />*/}
             </div>
           </div>
-
         </div>
       </header>
     );
---
+++ src/components/Course.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import querystring from 'querystring';
+
+const Course = props => {
+  const search = window.location.search;
+  const params = querystring.parse(search.split('?')[1]);
+  const tags = params.tags;
+  console.log(tags);
+  const myCourses = (
+    <div>
+      {props.courses.map(cert => {
+        return (cert.tags.includes(tags)) || tags === 'all' || !tags ? (
+          <div className="item" key={cert.name}>
+            <h3>
+              {cert.name} @ {cert.institution} <span>{cert.date}</span>
+              {cert.tags.split(',').map(tag => {
+                return (
+                  <span key={tag} className="label label-primary label-course">
+                    {tag}
+                  </span>
+                );
+              })}
+            </h3>
+            <p>{cert.description}</p>
+          </div>
+        ) : (
+          ''
+        );
+      })}
+    </div>
+  );
+  return (
+    <div className="title">
+      <i className="fa fa-trophy" />
+      <h2>Courses</h2>
+      {myCourses}
+    </div>
+  );
+};
+
+export default Course;
